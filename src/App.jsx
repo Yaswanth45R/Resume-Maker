95ebe57706ecdef2ac6fda2e0c483fcd4381a0c0
Yaswanth <pyaswanth4@gmail.com>
2024-02-05 12:49:20 +0530

    creates a template with an Header and Footer

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import '../styles/Header.scss'
+
+const Header = () =>{
+    return(
+        <header className='header'>
+             <h1 className='header__text'>Another CV Generator</h1>
+        </header>
+       
+    )
+}
+
+export default Header;
